d2355e25b822a9241bae87974fb5d8571d144a9b
Druidblack <70659424+Druidblack@users.noreply.github.com>
2024-08-14 20:57:43 +0300

    add .rstrip('/')

--- config.py
+++ config.py
@@ -1,4 +1,4 @@
 # config.py
 
-JELLYFIN_SERVER_URL = "http://192.168.1.161:8096"  # URL вашего сервера Jellyfin
+JELLYFIN_SERVER_URL = "http://192.168.1.161:8096".rstrip('/')  # URL вашего сервера Jellyfin
 API_KEY = "5ca0bf9a4b7a4ce89b3293f21ed8f5f2"  # Ваш API-ключ для аутентификации
